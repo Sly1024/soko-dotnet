8a78c781e6a6df0be75004d672390d2c97d89f41
Szilveszter Safar <szilveszter.safar@gmail.com>
2021-09-09 22:20:32 +0200

    Minor cleanup.

--- MoveRanges.cs
+++ MoveRanges.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 
 namespace soko
 {
@@ -54,62 +52,6 @@ namespace soko
             items[start].encoded = (ushort)firstFree[size];
             items[start+1].encoded = (ushort)(firstFree[size] >> 16);
             firstFree[size] = start;
-        }
-
-        public IEnumerable<Move> GetRangeAt(int idx) 
-        {
-            return new RangeEnumerator(this, idx);
-        }
-
-        private class RangeEnumerator : IEnumerable<Move>, IEnumerator<Move>
-        {
-            MoveRanges ranges;
-            int startIdx;
-            int currIdx;
-            bool wasLast = false;
-
-            public RangeEnumerator(MoveRanges ranges, int startIdx)
-            {
-                this.ranges = ranges;
-                this.startIdx = startIdx;
-                currIdx = startIdx-1;
-            }
-
-            public Move Current => ranges.items[currIdx];
-
-            object IEnumerator.Current => Current;
-
-            public void Dispose()
-            {
-            }
-
-            public bool MoveNext()
-            {
-                if (wasLast) {
-                    ranges.RemoveRange(startIdx, ++currIdx);
-                    return false;
-                }
-
-                ++currIdx;
-                wasLast = Current.IsLast;
-                return true;
-            }
-
-            public void Reset()
-            {
-                currIdx = startIdx-1;
-                wasLast = false;
-            }
-
-            public IEnumerator<Move> GetEnumerator()
-            {
-                return this;
-            }
-
-            IEnumerator IEnumerable.GetEnumerator()
-            {
-                return this;
-            }
         }
     }
 }
